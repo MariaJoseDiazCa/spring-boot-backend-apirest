6e2d9f91ed8583312702b7f409145174aebda29d
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2022-12-08 19:39:21 -0300

    UPDATE: add pagination

--- src/main/java/com/mjdiazc/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/mjdiazc/springboot/backend/apirest/models/services/IClienteService.java
@@ -2,10 +2,15 @@ package com.mjdiazc.springboot.backend.apirest.models.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.mjdiazc.springboot.backend.apirest.models.entity.Cliente;
 
 public interface IClienteService {
 	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
 	
 	public Cliente findById(Long id);
 	
